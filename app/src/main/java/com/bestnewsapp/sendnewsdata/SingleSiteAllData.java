64779a194435d14e0da7784da5a8a147a8425a33
Nixxmare <saurabh.joy8@gmail.com>
2017-02-02 04:01:38 +0530

    TOI News Extraction Done

--- app/src/main/java/com/bestnewsapp/sendnewsdata/SingleSiteAllData.java
+++ app/src/main/java/com/bestnewsapp/sendnewsdata/SingleSiteAllData.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -70,8 +72,9 @@ public class SingleSiteAllData extends AppCompatActivity {
 
 
             String url = "https://www.washingtonpost.com/opinions/";
-
-            Elements el, views = null;
+            String singleArticleTOI = "http://timesofindia.indiatimes.com/sports/ipl/news/ipl-10-to-start-on-april-5-existing-process-to-continue-for-vendor-deals/articleshow/56919653.cms";
+            String test2 = "http://timesofindia.indiatimes.com/india/next-month-volunteers-may-knock-on-your-door-for-cancer-diabetes-tests/articleshow/56696903.cms";
+            Elements el, views, toi = null;
             // String datato = "";
             String title = "";
             String image = "";
@@ -79,10 +82,11 @@ public class SingleSiteAllData extends AppCompatActivity {
             String author = "";
             String timeStamp = "";
             try {
-                doc = Jsoup.connect(url).get();
-                el = doc.select("div.story-headline");
-                title = el.toString();
-                Log.i("Data From the Site",el.toString() );
+                doc = Jsoup.connect(test2).get();
+//                el = doc.select("div.story-headline");
+                toi = doc.select("div.Normal");
+                title = toi.toString();
+                Log.i("Data From the Site",toi.toString() );
                 // int size = el.size();
 //                for (Element element : el) {
 //
@@ -102,7 +106,14 @@ public class SingleSiteAllData extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            allSiteData.setText(s);
+            Spanned result;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                result = Html.fromHtml(s, Html.FROM_HTML_MODE_LEGACY);
+            }
+            else {
+                result = Html.fromHtml(s);
+            }
+            allSiteData.setText(result);
             progressDialog.dismiss();
 
         }
