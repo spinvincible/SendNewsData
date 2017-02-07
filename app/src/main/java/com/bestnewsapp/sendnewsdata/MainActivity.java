8c8c544691e048d19ce613f20fcba7e7272a5d22
Nixxmare <spinvincible@users.noreply.github.com>
2017-02-07 10:55:57 +0530

    Update MainActivity.java

--- app/src/main/java/com/bestnewsapp/sendnewsdata/MainActivity.java
+++ app/src/main/java/com/bestnewsapp/sendnewsdata/MainActivity.java
@@ -34,3 +34,66 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
+// String timesOfIndia = "http://timesofindia.indiatimes.com/";
+
+//      doc = Jsoup.connect(timesOfIndia).get();
+// //            views = doc.select("ul[data-vr-zone]");
+// //            el = doc.select("[href]");
+// //            el = doc.select("img[src]");
+// //            System.out.println(el);
+// //                To get Text and then Extract the full source to it.
+// //                String extendFullLink;
+// //                String fullLink = "";
+// //                String imageLink = element.select("img[src]").attr("abs:src");
+// //                if (!imageLink.isEmpty()) {
+// //                    extendFullLink = element.select("img[src]").attr("data-src");
+// //                    if (!extendFullLink.isEmpty()) {
+// //                        fullLink = imageLink + extendFullLink;
+// //                        System.out.println(fullLink);
+// //
+// //                    }
+// //                }
+
+//             searchLinks = doc.select("a[pg*=city],[pg*=new_latest],[pg*=new_tops]" +
+//                     ",[pg*=special],[pg*=cricket],[pg*=entertainment_Movie],[pg*=education],[pg*=world],[pg*=mostPopular]")
+//                     .not("a[pg*=nav]").not("a[pg*=speak]").not("a[style]").not("span").not("a[class]").not("a[rel]").not("img");
+// //            System.out.println(searchLinks.toString());
+
+// //            searchContent = doc.select("span.time_cptn");
+// //            System.out.println(searchLinks.text());
+//             int size = searchLinks.size();
+//             System.out.println(size);
+//             for (Element element : searchLinks) {
+//                 ArticleData articleData;
+//                 urlPost = element.absUrl("href");
+//                 title = element.attr("title");
+// //                internalData = Jsoup.connect(urlPost).get();
+// //                img_url= internalData.select("img[data-imgid]").attr("abs:src");
+// //                content = internalData.select("div.Normal").toString();
+// //                System.out.println(searchContent);
+
+//                 if(!title.isEmpty()&&!urlPost.isEmpty()){
+//                     articleData = new ArticleData(title, urlPost);
+//                     fullValidList.add(articleData);
+//                 }
+
+
+//             }
+
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+
+//         Set<ArticleData> uniqueData = new LinkedHashSet<>();
+//         uniqueData.addAll(fullValidList);
+//         fullValidList.clear();
+//         fullValidList.addAll(uniqueData);
+
+//         for (ArticleData aFullValidList : fullValidList) {
+//             System.out.println(aFullValidList.getTitle());
+//             System.out.println(aFullValidList.getUrl());
+//         }
+
+//     }
